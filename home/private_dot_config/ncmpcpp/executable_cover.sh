54fe0f87606b84ba3e13f0bd7f1f017dea538a74
theokrueger <theokrueger@airmail.cc>
2025-03-17 22:01:55 -0400

    fix cover obs for sway

--- home/private_dot_config/ncmpcpp/executable_cover.sh
+++ home/private_dot_config/ncmpcpp/executable_cover.sh
@@ -7,14 +7,19 @@ fi
 
 INFILE="/tmp/cover_path"
 function add_cover {
-  echo '{"action": "add", "identifier": "cover","x": 3, "y": 1, "max_width": 25, "max_height": 25, "path":"'$(cat $INFILE)'"}'
+  echo '{"action": "add", "identifier": "cover","x": 2, "y": 0, "max_width": 25, "max_height": 13, "path":"'$(cat $INFILE)'"}'
+}
+
+# TODO this is a hack
+function add_cover_if_applicable {
+  if [[ $(swaymsg -t get_tree | jq '.. | (.nodes? // empty)[] | select(.focused==true) | .app_id') == '"Alacritty"' ]]; then
+    add_cover
+  fi
 }
 
 ueberzugpp layer -o wayland 0< <(
   add_cover
   while inotifywait -q -q -e close_write "$INFILE"; do
-    if [[ $(hyprctl activewindow | grep Alacritty) != '' ]]; then
-        add_cover
-    fi;
+    add_cover_if_applicable
   done
 )
