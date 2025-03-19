e89d0d550b02305f904cd7abbc55822791562ae0
theokrueger <theokrueger@airmail.cc>
2025-03-19 19:25:28 +0000

    switch to nnn

---
+++ home/private_dot_config/nnn/executable_nnn.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# launch nnn with settings
+PLUG_DOWNLOADED_FILE="$HOME/.config/nnn/plugins-update"
+if ! [[ -e "$PLUG_DOWNLOADED_FILE" ]]; then
+  echo "Downloading Plugins"
+  sh -c "$(curl -Ls https://raw.githubusercontent.com/jarun/nnn/master/plugins/getplugs)"
+  touch "$PLUG_DOWNLOADED_FILE"
+fi
+
+export NNN_TRASH=1
+export NNN_COLORS="2136"
+export NNN_OPTS="adEx"
+export NNN_PLUG="g:preview-tui"
+export NNN_TERMINAL="alacritty"
+nnn
